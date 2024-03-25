ded382829306b33411107b02dc1a9e92b7024dec
igorskiter <igor.nw3@gmail.com>
2024-03-25 19:16:10 -0300

    feat: ajusting positions components

--- components/section-carousel.tsx
+++ components/section-carousel.tsx
@@ -1,9 +1,14 @@
 "use client";
 import type { NextPage } from "next";
-import { memo, useEffect } from "react";
+import { memo, useCallback, useEffect, useRef, useState } from "react";
 import styles from "./section-carousel.module.css";
 
 const SectionCarousel: NextPage = memo(() => {
+  const pilarsRef = useRef([]);
+  const [beforePosition, setBeforePosition] = useState(-1);
+  const [position, setPosition] = useState(0);
+  const [nextPosition, setNextPosition] = useState(1);
+  const [initialPosition, setInitialPosition] = useState({});
   const pilars = [
     "diet",
     "social",
@@ -14,23 +19,36 @@ const SectionCarousel: NextPage = memo(() => {
   ];
 
   const pilarsDescription = {
-    diet: "From nutrition and physical activity to sleep patterns and stress management, we delve  into various aspects of your daily life.",
-    social:
-      "Strong social connections can improve our mental health and increase longevity, while loneliness and isolation can lead to a decline in mental and physical health.",
-    controltoxic:
-      "Excessive use of substances like alcohol, tobacco, or drugs can lead to a range of health problems, including heart disease, liver disease, respiratory problems, and mental health disorders.",
-    stressmanagement:
-      "Chronic stress can negatively affect both our physical and mental increase longevity leading to conditions and isolation can lead to a decline in mental and physical health.",
-    sleepquality:
-      "Quality sleep is essential for our body’s to repair processes, cognitive function, and overall well-being. Chronic sleep deprivation can lead to various health issues, including heart disease, obesity, and depression.",
-    physicalactivities:
-      "Regular physical activity strengthens our heart and muscles, improves mental health, and reduces the risk of many health conditions, including heart disease and diabetes.",
+    diet: {
+      description:
+        "The food we consume directly impacts our physical health, affecting everything from our energy levels and body weight to our risk of developing chronic diseases.",
+    },
+    social: {
+      description:
+        "Strong social connections can improve our mental health and increase longevity, while loneliness and isolation can lead to a decline in mental and physical health.",
+    },
+    controltoxic: {
+      description:
+        "Use of substances like alcohol, tobacco, or drugs can lead to a range of health problems, including heart disease, liver disease, respiratory problems, and mental health disorders.",
+    },
+    stressmanagement: {
+      description:
+        "Chronic stress can negatively affect both our physical and mental increase longevity leading to conditions and isolation can lead to a decline in mental and physical health.",
+    },
+    sleepquality: {
+      description:
+        "Quality sleep is essential for our body’s to repair processes, cognitive function, and overall well-being. Chronic sleep deprivation can lead to various health issues, including heart disease, obesity, and depression.",
+    },
+    physicalactivities: {
+      description:
+        "Regular physical activity strengthens our heart and muscles, improves mental health, and reduces the risk of many health conditions, including heart disease and diabetes.",
+    },
   };
 
   const moveIndicesToEnd = (arr: string[], index: number): string[] => {
     if (index < 0 || index >= arr.length) {
-      console.error("Índice inválido");
-      return arr;
+      console.log(index, "moveIndicesToEnd");
+      index = 0;
     }
 
     const firstPart = arr.slice(0, index);
@@ -41,100 +59,151 @@ const SectionCarousel: NextPage = memo(() => {
   };
 
   useEffect(() => {
-    if (typeof window !== "undefined" && window.IntersectionObserver) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const indexFirstPilar = pilars.indexOf(entry.target.id);
-            const newOrderPilar = moveIndicesToEnd(pilars, indexFirstPilar);
-
-            // document
-            //   .getElementById(newOrderPilar[0])
-            //   ?.classList.add(styles["showHidden"]);
-            // console.log(entry.intersectionRatio, entry.target.id);
-            if (entry.intersectionRatio >= 0.3 && entry.isIntersecting) {
-              newOrderPilar.forEach((res, index) => {
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position0"]);
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position1"]);
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position2"]);
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position3"]);
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position4"]);
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position5"]);
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.remove(styles["position6"]);
-
-                document
-                  .getElementById(`${res}Icon`)
-                  ?.classList.add(styles[`position${index}`]);
-
-                if (index === 0) {
-                  document
-                    .getElementById(`${res}`)
-                    ?.classList.add(styles["showHidden"]);
-
-                  var scrollDiv: number =
-                    document.getElementById(newOrderPilar[0])?.offsetTop || 0;
-                  console.log(scrollDiv, res, newOrderPilar[0]);
-                  setTimeout(() => {
-                    // document.body.scrollTo({
-                    //   top: scrollDiv,
-                    //   behavior: "smooth",
-                    // });
-                  }, 300);
-                  return;
-                }
-                document
-                  .getElementById(`${res}`)
-                  ?.classList.remove(styles["showHidden"]);
-              });
-              return;
-            }
-          });
-        },
-        {
-          threshold: [0, 0.3, 1],
-        }
-      );
-      if (observer) {
-        pilars.forEach((pilar) => {
-          observer.observe(document.getElementById(pilar) as HTMLElement);
-        });
-        // const oberverPilares = new IntersectionObserver(
-        //   (entries) => {
-        //     console.log(entries[0]);
-        //     if (entries[0]?.isIntersecting) {
-        //       var scrollDiv: number =
-        //         document.getElementById("diet")?.offsetTop || 0;
-        //       console.log(scrollDiv);
-        //       document.body.scrollTo({
-        //         top: scrollDiv,
-        //         behavior: "smooth",
-        //       });
-        //     }
-        //   },
-        //   {
-        //     threshold: [0, 0.3, 1],
-        //   }
-        // );
-        // oberverPilares.observe(
-        //   document.getElementById("pilars") as HTMLElement
-        // );
-      }
-    }
+    pilars.forEach((res, index) => {
+      const elmnt = pilarsRef[index];
+      console.log(elmnt.offsetTop, ".offsetTop");
+      setInitialPosition((prevState) => ({
+        ...prevState,
+        [res]: elmnt.offsetTop,
+      }));
+    });
   }, []);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const onScroll = () => {
+      if (
+        typeof window !== "undefined" &&
+        pilarsRef[0] &&
+        pilarsRef[0].getBoundingClientRect
+      ) {
+        let { top, height } = pilarsRef[0].getBoundingClientRect();
+        let direction = lastScrollY < window.scrollY ? "down" : "up";
+        lastScrollY = window.scrollY;
+
+        const positionDown =
+          -(top / height) <= 0 ? 0 : -(top / height) > 5 ? 5 : -(top / height);
+
+        const positionRound = +Math.round(positionDown);
+
+        console.log(
+          top,
+          height,
+          beforePosition,
+          positionRound,
+          nextPosition,
+          "direction"
+        );
+
+        if (
+          window.innerWidth < 1024 &&
+          top >= 0 &&
+          top <= 300 &&
+          beforePosition === -1 &&
+          positionRound === 0 &&
+          nextPosition === 1 &&
+          direction === "down"
+        ) {
+          console.log("GO TO DIEAT");
+          window.scrollTo({
+            behavior: "smooth",
+            top: initialPosition["diet"],
+          });
+        }
+
+        if (
+          window.innerWidth < 1024 &&
+          top >= 0 &&
+          top <= 130 &&
+          beforePosition === -1 &&
+          positionRound === 0 &&
+          nextPosition === 1 &&
+          direction === "up"
+        ) {
+          console.log("GO TO DIEAT");
+          window.scrollTo({
+            behavior: "smooth",
+            top: initialPosition["diet"] - height,
+          });
+        }
+
+        if (
+          positionRound !== nextPosition ||
+          positionRound !== beforePosition
+        ) {
+          console.log("direction CHANGE");
+          setPosition((prevState) => {
+            if (positionRound === prevState) {
+              return prevState;
+            }
+            return positionRound;
+          });
+        }
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [beforePosition, position, nextPosition]);
+
+  useEffect(() => {
+    console.log("enter position", beforePosition, position, nextPosition);
+    changePosition(position);
+    setBeforePosition(position - 1 >= -1 ? position - 1 : -1);
+    setNextPosition(position + 1 <= 5 ? position + 1 : 5);
+  }, [position]);
+
+  const changePosition = useCallback(
+    (position: number) => {
+      const newOrderPilar = moveIndicesToEnd(pilars, position);
+
+      newOrderPilar.forEach((res, index) => {
+        document.getElementById(`${res}`)?.classList.add(styles["showHidden"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position0"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position1"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position2"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position3"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position4"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position5"]);
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.remove(styles["position6"]);
+
+        document
+          .getElementById(`${res}Icon`)
+          ?.classList.add(styles[`position${index}`]);
+
+        document
+          .getElementById(`${res}`)
+          ?.classList.remove(styles["showHidden"]);
+
+        if (index === 0) {
+          document
+            .getElementById(`${res}`)
+            ?.classList.add(styles["showHidden"]);
+
+          window.scrollTo({
+            behavior: "smooth",
+            top: initialPosition[res],
+          });
+        }
+      });
+    },
+    [initialPosition]
+  );
 
   return (
     <section className={styles.sectioncarousel} id={"SectionCarousel"}>
@@ -233,15 +302,41 @@ const SectionCarousel: NextPage = memo(() => {
           </div>
         </div>
         <div className={styles.pilars} id="pilars">
-          {Object.keys(pilarsDescription).map((pilar) => (
+          <div
+            className={`${styles.box} ${styles.initHidden} ${styles.pilarContainerIntro}`}
+          >
+            <div className={`${styles.pilarContainer}`}>
+              <p
+                className={`${styles.understandAct} ${styles.understandActTitle}`}
+              >
+                From nutrition and physical activity to sleep patterns and
+                stress management, we delve into various aspects of your daily
+                life
+              </p>
+              <p className={styles.understandAct}>
+                With this knowledge, it is possible to identify potential risks
+                early, make informed decisions, and transition from reactive
+                healthcare to proactive, preventive care.
+              </p>
+            </div>
+          </div>
+          {Object.keys(pilarsDescription).map((pilar, index) => (
             <div
+              ref={(ref) => (pilarsRef[index] = ref)}
               className={`${styles.box} ${styles.initHidden}`}
               key={pilar}
               id={pilar}
             >
-              <div className={styles.fromNutritionAndContainer}>
+              <div className={styles.pilarContainer}>
+                {pilarsDescription[pilar].title && (
+                  <p
+                    className={`${styles.understandAct} ${styles.understandActTitle}`}
+                  >
+                    {pilarsDescription[pilar].title}
+                  </p>
+                )}
                 <p className={styles.understandAct}>
-                  {pilarsDescription[pilar]}
+                  {pilarsDescription[pilar].description}
                 </p>
               </div>
             </div>
